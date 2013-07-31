8e223973d0238c18d7ada84b2cbf287a4e6812e6
Tatsuya Saito <twopackas@gmail.com>
2013-07-31 20:14:45 +0900

    さらにリファクタリング。
    1-1-3の場合は二等辺三角形ではない。

--- myers_triangle/js/triangle.js
+++ myers_triangle/js/triangle.js
@@ -11,17 +11,24 @@ MyersTriangle.prototype = {
         if(a == b && b == c) {
             return EQUILATERAL;
         }
-        if(this.isIsosceles(a, b, c) ||
-           this.isIsosceles(b, c, a) ||
-           this.isIsosceles(c, a, b)) {
+        if(this.isIsosceles(a, b, c)) {
             return ISOSCELES;
         }
         return NOT_TRIANGLE;
     },
     
     isIsosceles : function(a, b, c) {
-        if(a == b) {
-            if(a + b > c) {
+        if(this.isIsoscelesXeqY(a, b, c) ||
+           this.isIsoscelesXeqY(b, c, a) ||
+           this.isIsoscelesXeqY(c, a, b)) {
+            return true;
+        }
+        return false;
+    },
+
+    isIsoscelesXeqY : function(x, y, z) {
+        if(x == y) {
+            if(x + y > z) {
                 return true;
             }
         }
